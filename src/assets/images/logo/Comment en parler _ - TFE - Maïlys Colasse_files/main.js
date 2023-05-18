e2f8ad6a1526eb9cb3f2532865d1b2c8d702ef76
mailysmae <79702008+mailysmae@users.noreply.github.com>
2023-05-18 14:38:59 +0200

    FEAT: css

---
+++ src/assets/images/logo/Comment en parler _ - TFE - Maïlys Colasse_files/main.js
@@ -0,0 +1,31 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!*****************************!*\
+  !*** ./src/scripts/main.js ***!
+  \*****************************/
+
+let Button = document.querySelector(".menu__button");
+let deroulant = document.querySelector(".open");
+
+Button.addEventListener("click", toggleNavigation);
+deroulant.addEventListener("click", toggle);
+
+
+function toggleNavigation(){
+    if(document.body.hasAttribute("burger-menu")){
+        document.body.removeAttribute("burger-menu");
+        Button.innerHTML = "Menu";
+    }else{
+        document.body.setAttribute("burger-menu", true);
+        Button.innerHTML = "Retour";
+    }
+}
+function toggle(){
+ let ouverte = document.querySelector(".sousrubrique");
+  ouverte.classList.toggle("close");
+}
+
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
